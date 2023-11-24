b83c1e20ff45f74d3f84e6829ea5b9b0312845a0
prantosheikh <prantosheikh834@gmail.com>
2023-11-24 23:52:41 +0600

    backend and frontend api properly working

--- index.js
+++ index.js
@@ -31,6 +31,8 @@ async function run() {
     // Send a ping to confirm a successful connection
 
 
+    
+
     app.post('/jwt', (req, res) => {
       const user = req.body;
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: "1h"})
